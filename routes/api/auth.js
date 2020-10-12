c24c7b1202894a74dd0cbfdf227857fb0874991b
GITWizard3000 <72543669+GITWizard3000@users.noreply.github.com>
2020-10-12 14:28:17 +0200

    Add files via upload

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -73,7 +73,7 @@ router.post('/', [
              { expiresIn: 3600000},
              (err, token) => {
                  if(err) throw err;
-                 res.json({ token});
+                 res.json({token});
              });
 
     } catch(err) {
